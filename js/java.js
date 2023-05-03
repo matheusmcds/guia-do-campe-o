248944545b0f35d2bb2e5ab8d211376d09b417d7
matheusmcds <119543235+matheusmcds@users.noreply.github.com>
2023-05-03 20:00:12 -0300

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,10 @@
+Document.querySelector('#form-login').addEventListener('submit', e=>{
+
+e.preventDefault();
+
+let email = document.querySelector('#email').value;
+let senha = document.querySelector('#senha').value;
+
+
+
+})
